c1aafc55629c3d2d7549a848ba0c3e394934201c
raaghavendrahm <raaghavendrahm@gmail.com>
2021-10-27 16:06:26 +0530

    fetchVacancies function is created to fetch vacancies data from "db.json" and setVacancies is used to set the state accordingly.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,10 +8,25 @@ const Home = () => {
   // Initial state for vacancies is set to empty array:
   const [vacancies, setVacancies] = useState([]);
 
+  // Fetch Vacancies (with server):
+
+  // With initial component lodading, data (jobs) from "db.json" is fetched and the state is set to data (jobs) using setVacancies and useEffect hook:
+
+  useEffect(() => {
+    const fetchVacancies = async () => {
+      const res = await fetch('http://localhost:5000/vacancies');
+      const vacancies = await res.json();
+      setVacancies(vacancies);
+    };
+
+    fetchVacancies();
+  }, []); // empty dependency array denoting effect on initial component loading.
+
+  /* // Before Server:
   // With initial page loading, data (jobs) from 'data' is set as state for vacancies using 'setVacancies':
   useEffect(() => {
     setVacancies(jobs);
-  }, []); // empty array dependecy
+  }, []); // empty array dependecy */
 
   // Add Vacancy
   const addVacancy = (vacancy) => {
