ac83fa9218d8624bba97bc91f40502681fc759b8
raaghavendrahm <raaghavendrahm@gmail.com>
2021-10-27 16:36:12 +0530

    "Jobs" page is updated with vacancies data with apply and delete functionality.

--- src/pages/Jobs.js
+++ src/pages/Jobs.js
@@ -1,5 +1,62 @@
+import { useState, useEffect } from 'react';
+import Vacancy from '../components/Vacancy';
+
 const Jobs = () => {
-  return <div>This is jobs page</div>;
+  // Initial state for vacancies is set to empty array:
+  const [vacancies, setVacancies] = useState([]);
+
+  // Fetch Jobs (with server)
+
+  // With initial component lodading, data (jobs) from "db.json" is fetched and the state is set to data (jobs) using setVacancies and useEffect hook:
+  useEffect(() => {
+    const fetchJobs = async () => {
+      const res = await fetch('http://localhost:5000/vacancies');
+      const data = await res.json();
+      setVacancies(data);
+    };
+
+    fetchJobs();
+  }, []);
+
+  // Delete Vacancy (with server)
+
+  // To delete a vacancy not only in UI, but on server too:
+  const deleteVacancy = async (id) => {
+    await fetch(`http://localhost:5000/vacancies/${id}`, {
+      method: 'DELETE',
+    });
+    setVacancies(vacancies.filter((vacancy) => vacancy.id !== id));
+  };
+
+  // Apply for Vacancy
+  const applyVacancy = (vacancy) => {
+    window.alert(`Successfully applied for the position ${vacancy.title}`);
+  };
+
+  return (
+    <>
+      <section className="hero">
+        <div className="hero-body">
+          <p className="title">Find your dream job today</p>
+          <p className="subtitle">Apply for any vacancies below</p>
+        </div>
+      </section>
+
+      {/* Display vacancies: */}
+      <section>
+        {vacancies.map((vacancy) => {
+          return (
+            <Vacancy
+              key={vacancy.id}
+              vacancy={vacancy}
+              deleteVacancy={deleteVacancy}
+              applyVacancy={applyVacancy}
+            />
+          );
+        })}
+      </section>
+    </>
+  );
 };
 
 export default Jobs;
